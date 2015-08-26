f8f2d6c4c58f64c9e320ac877e92397cbc92c2d4
Wei Dai <weidai@Appfolios-MacBook-Pro.local>
2015-08-26 16:16:58 -0700

    Flix app

---
+++ flix_original/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flix::Application.config.secret_key_base = '8329957dc06aec20b061266677ab3617edbb7151360219ce518f1af3cb20b661d991910c907abe8674471a6bbc37b94e90b27b823b079d5dfaf36ce7a059fa72'
